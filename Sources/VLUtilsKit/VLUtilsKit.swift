3c74d7e86fc7724f3d2f9c85b900f4efc0c78962
VLstack <github@vlstack.fr>
2023-12-05 13:12:08 +0100

    deprecate global func "animate"

--- Sources/VLUtilsKit/VLUtilsKit.swift
+++ Sources/VLUtilsKit/VLUtilsKit.swift
@@ -18,6 +18,7 @@ extension NSAccessibility
 /// This function sets the given `Animation` as the `Transaction/animation`
 /// property of the thread's current `Transaction`
 /// if "Reduce Motion" setting is "on" this function returns the provided View body
+@available(*, deprecated, message: "use VLUtils.animate instead")
 public
 func animate<Result>(_ animation: Animation? = .default,
                      _ body: () throws -> Result) rethrows -> Result
